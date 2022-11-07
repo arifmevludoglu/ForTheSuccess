c827ce4baaf36bcd71d247d1c82ea6768cdd3994
eemmrree <eemmrree0@outlook.com>
2022-11-07 20:17:39 +0300

    minör düzeltme

--- ForTheSuccess.Domain/Entities/BaseEntity.cs
+++ ForTheSuccess.Domain/Entities/BaseEntity.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using ForTheSuccess.Domain.Enums;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ForTheSuccess.Domain.Entities
 {
-    internal class BaseEntity
+    public abstract class BaseEntity
     {
+        public int CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public int LastUpdatedBy { get; set; }
+        public DateTime? LastUpdatedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public DateTime? DeletedDate { get; set; }
+        public EuIsDeleted IsDeleted { get; set; } = EuIsDeleted.No;
     }
 }
